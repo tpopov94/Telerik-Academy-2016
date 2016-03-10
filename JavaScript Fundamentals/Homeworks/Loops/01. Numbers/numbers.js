872839f36a6dec464d075261df6ef3f8050d457d
tpopov94 <t.popov94@gmail.com>
2016-03-10 21:17:59 +0200

    Updated Loops Homework numbers.js

---
+++ JavaScript Fundamentals/Homeworks/Loops/01. Numbers/numbers.js
@@ -0,0 +1,29 @@
+function printNumbers() {
+    'use strict';
+
+    var n = +document.getElementById('inputForm').value,
+        i;
+
+    if(isNaN(n)) {
+        window.console.log('Please enter a valid number');
+        document.getElementById('console').innerHTML += 'Please enter a valid number' + '<br>';
+    }
+
+    else {
+        for (i = 1; i <= n; i+= 1) {
+            window.console.log(i);
+            document.getElementById('console').innerHTML += i + '<br>';
+        }
+
+        /* With while loop
+
+         i = 1;
+         while(i <= n) {
+            window.console.log(i);
+            document.getElementById('console').innerHTML += i + '<br>';
+         i+= 1;
+         }
+        * */
+
+    }
+}
