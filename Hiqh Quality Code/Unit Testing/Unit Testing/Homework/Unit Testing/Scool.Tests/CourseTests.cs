e0fedc38df1161b4db8735f4635ca1cda293b3ac
tpopov94 <t.popov94@gmail.com>
2016-07-18 18:37:01 +0300

    Added CourseTests.cs

---
+++ Hiqh Quality Code/Unit Testing/Unit Testing/Homework/Unit Testing/Scool.Tests/CourseTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Scool.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using School;
+
+    [TestClass]
+    public class CourseTests
+    {
+        private Course sampleCourse = new Course("Unit Testing");
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddingNullTitleShouldThrowArgumentNullException()
+        {
+            var course = new Course(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddingEmptyTitleShouldThrowArgumentNullException()
+        {
+            var course = new Course("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddintNullStudentToStudentsListShouldThrowNewArgumentNullException()
+        {
+            sampleCourse.AddStudent(new Student(null, "Sigurdson"));
+        }
+
+        [TestMethod]
+        public void AddingNewStudentToTheCourseShouldIncreaseStundentsInCourseByOne()
+        {
+            sampleCourse.AddStudent(new Student("Pesho", "Peshovson"));
+
+            Assert.AreEqual(1, sampleCourse.StudentsList.Count, "Students list is not updated correctly");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void AddingTheSameStudentTwiceInOneCourseShouldThrowApplicationException()
+        {
+            var ragnarStudent = new Student("Ragnar", "Peshovski");
+            sampleCourse.AddStudent(ragnarStudent);
+            sampleCourse.AddStudent(ragnarStudent);
+        }
+    }
+}
