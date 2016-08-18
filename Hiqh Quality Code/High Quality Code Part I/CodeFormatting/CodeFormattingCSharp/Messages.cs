418f2504e0c9c90ef1f33e7dfc44a53480f40158
tpopov94 <t.popov94@gmail.com>
2016-08-18 19:14:07 +0300

    Added CodeFormatting Homework

---
+++ Hiqh Quality Code/High Quality Code Part I/CodeFormatting/CodeFormattingCSharp/Messages.cs
@@ -0,0 +1,41 @@
+﻿namespace CodeFormattingCSharp
+{
+    using System.Text;
+
+    public static class Messages
+    {
+        private static StringBuilder output = new StringBuilder();
+
+        public static StringBuilder Output { get; }
+
+        public static void EventAdded()
+        {
+            Output.Append("Event added\n");
+        }
+
+        public static void EventDeleted(int x)
+        {
+            if (x == 0)
+            {
+                NoEventsFound();
+            }
+            else
+            {
+                Output.AppendFormat("{0} events deleted\n", x);
+            }
+        }
+
+        public static void NoEventsFound()
+        {
+            Output.Append("No events found\n");
+        }
+
+        public static void PrintEvent(Event eventToPrint)
+        {
+            if (eventToPrint != null)
+            {
+                Output.Append(eventToPrint + "\n");
+            }
+        }
+    }
+}
