68e08ce18d43f550a7815bf41d03910e7f0e5f25
tpopov94 <t.popov94@gmail.com>
2016-08-19 22:21:04 +0300

    Added MineSweeper.cs refactored names homework

---
+++ Hiqh Quality Code/High Quality Code Part I/NamingIdentifiers/MineSweeper/Points.cs
@@ -0,0 +1,44 @@
+﻿namespace MineSweeper
+{
+    public class Points
+    {
+        private string name;
+        private int points;
+
+        public Points()
+        {
+        }
+
+        public Points(string name, int points)
+        {
+            this.PlayerName = name;
+            this.PlayerPoints = points;
+        }
+
+        public string PlayerName
+        {
+            get 
+            {
+                return this.name;
+            }
+
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public int PlayerPoints
+        {
+            get
+            {
+                return this.points;
+            }
+
+            set
+            {
+                this.points = value;
+            }
+        }
+    }
+}
