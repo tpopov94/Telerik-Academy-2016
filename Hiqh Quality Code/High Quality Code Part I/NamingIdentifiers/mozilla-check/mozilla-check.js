644e7f12ee7b17f28e5f2836134cccf582eabf98
tpopov94 <t.popov94@gmail.com>
2016-08-19 12:22:33 +0300

    Added NamingIdentifiers.sln

---
+++ Hiqh Quality Code/High Quality Code Part I/NamingIdentifiers/mozilla-check/mozilla-check.js
@@ -0,0 +1,11 @@
+﻿function checkIfMozilla(event, arguments) {
+    var myWindow = window,
+        browser = myWindow.navigator.appCodeName,
+        isMozilla = (browser === "Mozilla");
+
+    if (isMozilla) {
+        alert("Yes");
+    } else {
+        alert("No");
+    }
+}
