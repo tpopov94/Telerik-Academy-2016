bd40848c47565a37b395b0a350d6f4ddfb1cd3b5
tpopov94 <t.popov94@gmail.com>
2016-10-31 11:27:33 +0200

    Started EntityFramework Homework

---
+++ Databases/11. EF/EntityFramework/01.NorthwindDbContext/Start.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01.NorthwindDbContext
+{
+    public class Start
+    {
+        static void Main()
+        {
+            using (var db = new NorthwndEntities())
+            {
+                var categories = db.Categories;
+
+                foreach (var category in categories)
+                {
+                    Console.WriteLine(category.CategoryID + ": " + category.CategoryName);
+                }
+            }
+        }
+    }
+}
